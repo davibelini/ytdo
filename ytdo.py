8b17f23bd80f8b9045c0dfd865c8430428d68bd7
davibelini <dev.dado.belini@gmail.com>
2020-12-11 08:34:40 -0300

    getchanneldescription command added

--- ytdo.py
+++ ytdo.py
@@ -5,18 +5,41 @@ from setup import api_key
 def parse_quotes(string):
   return string.replace("\"", "")
 
+youtube = build('youtube', 'v3', developerKey=api_key)
+
 if len(sys.argv) >= 2:
   command = sys.argv[1]
   channel_name = parse_quotes(sys.argv[2])
   if command == 'getsubscribers':
-    youtube = build('youtube', 'v3', developerKey=api_key)
     request = youtube.channels().list(
       part='statistics',
       forUsername=channel_name
     )
     response = request.execute()
-    subscribers_number = response["items"][0]["statistics"]["subscriberCount"]
-    print(subscribers_number)
+    print(response)
+    if "items" in response:
+      subscribers_number = response["items"][0]["statistics"]["subscriberCount"]
+      print(subscribers_number)
+    else:
+      print("ERROR: couldn't get data")
+    quit()
+  elif command == 'getchannelviews':
+    request = youtube.channels().list(
+      part='statistics',
+      forUsername=channel_name
+    )
+    response = request.execute()
+    views_number = response["items"][0]["statistics"]["viewCount"]
+    print(views_number)
+    quit()
+  elif command == 'getchanneldescription':
+    request = youtube.channels().list(
+      part=["snippet", "contentDetails", "statistics"],
+      forUsername=channel_name
+    )
+    response = request.execute()
+    views_number = response["items"][0]["snippet"]["description"]
+    print(views_number)
     quit()
 else:
   print('ERROR: too many few arguments')
